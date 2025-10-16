ca4e909ecd8cc277a32377d2420ca323301fbfa0
SpencerHawk21 <111367952+Legs21@users.noreply.github.com>
2025-10-16 15:09:58 -0600

    Update links and content in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,28 +78,41 @@ export default function Page() {
               href="/dashboard/aboutme"
               className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
             >
-              <span>About me</span> <ArrowRightIcon className="w-5 md:w-6" />
+              <span>About me!</span>
             </Link>
         </div>
         <div className="place-self-end gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-              <strong>Welcome to my website.</strong> Here I will document and catalog my backcountry ski and snowboard trips.
+              <strong>Map!</strong>
             </p>
             <Link
-              href="/dashboard/aboutme"
+              href="/dashboard/map"
               className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
             >
-              <span>About me</span> <ArrowRightIcon className="w-5 md:w-6" />
+              <span>Check out the map!</span>
             </Link>
         </div>
-        <div className="bg-black h-40 mt-4 gap-4 place-self-end max-w-2/3">
-          <p className="text-xl text-white float-right p-4">Test words and extra stuff 1</p>
+        <div className="place-self-start gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+            <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+              <strong>Here is where I blog my trips.</strong>
+            </p>
+            <Link
+              href="/dashboard/trips"
+              className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Trips!</span>
+            </Link>
         </div>
-        <div className="bg-black h-40 mt-4 gap-4 place-self-start max-w-180">
-          <p className="text-xl text-white float-left p-4">Test words and extra stuff 2</p>
-        </div>
-        <div className="bg-black h-40 mt-4 gap-4 place-self-end max-w-500">
-          <p className="text-xl text-white float-left p-4">Test words and extra stuff 3</p>
+        <div className="place-self-end gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+            <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+              <strong>Gear!</strong>
+            </p>
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Main Dashboard</span>
+            </Link>
         </div>
       </div>
     </main>
