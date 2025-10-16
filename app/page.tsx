dd8f2424eed03d0211c41426db78b74f59e4bdde
SpencerHawk21 <111367952+Legs21@users.noreply.github.com>
2025-10-16 14:17:11 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Page() {
       </div>
       
       <div className="grid grid-col-5 mt-4 bg-cover bg-no-repeat bg-center bg-clip-border p-5 bg-[url(/mayflower_gulch.jpg)] gap-4 md:flex-col">
-        <div className="col-start-3 col-end-4 gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="col-start-1 col-end-3 gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
               <strong>Welcome to my website.</strong> Here I will document and catalog my backcountry ski and snowboard trips.
             </p>
