70b8b7eb91fb668091bb012368ee15af3d5b7bb6
SpencerHawk21 <111367952+Legs21@users.noreply.github.com>
2025-10-16 13:29:43 -0600

    Refactor welcome section and remove unused divs

--- app/page.tsx
+++ app/page.tsx
@@ -69,16 +69,17 @@ export default function Page() {
       </div>
       <div className="mt-4 grid-cols-5 bg-cover bg-no-repeat bg-center bg-clip-border p-5 bg-[url(/mayflower_gulch.jpg)] gap-4 md:flex-row">
         <div className="col-start-01 col-span-3 flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to my website.</strong> Here I will document and catalog my backcountry ski and snowboard trips.
-          </p>
-          <Link
-            href="/dashboard/aboutme"
-            className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>About me</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+            <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+              <strong>Welcome to my website.</strong> Here I will document and catalog my backcountry ski and snowboard trips.
+            </p>
+            <Link
+              href="/dashboard/aboutme"
+              className="flex items-center gap-5 self-start rounded-lg bg-[#2F5D50] px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>About me</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+          </div>
         </div>
         {/* <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           Add Hero Images Here
@@ -90,51 +91,15 @@ export default function Page() {
         />
         </div> */}
         <div className="col-start-3 col-span-3">
-      <div className="my-8">
-        <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-      </div>
-      <div className="col-start-1 col-span-3">
-        <div className="flex h-56 w-full items-center justify-center rounded bg-stone-200"><svg width="1.5em" height="1.5em" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor" className="h-16 w-16 text-surface-dark"><path d="M21 3.6V20.4C21 20.7314 20.7314 21 20.4 21H3.6C3.26863 21 3 20.7314 3 20.4V3.6C3 3.26863 3.26863 3 3.6 3H20.4C20.7314 3 21 3.26863 21 3.6Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 16L10 13L21 18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path><path d="M16 10C14.8954 10 14 9.10457 14 8C14 6.89543 14.8954 6 16 6C17.1046 6 18 6.89543 18 8C18 9.10457 17.1046 10 16 10Z" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+          <div className="my-8">
+            <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
+            <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
+            <div className="mb-2 h-2 rounded-full bg-stone-200"></div>
+            <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
+            <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
+            <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
+          </div>
         </div>
-      </div>
-      <div className="my-8">
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-      </div>
-      <div className="my-8">
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-      </div>
-      <div className="my-8">
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-      </div>
-      <div className="my-8">
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-        <div className="mb-2 h-2 w-full rounded-full bg-stone-200"></div>
-      </div>
-    </div>
       </div>
     </main>
   );
