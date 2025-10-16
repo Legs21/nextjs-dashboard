7030dccde0f24c0ae5c62af0907b33307e22e48f
SpencerHawk21 <111367952+Legs21@users.noreply.github.com>
2025-10-16 14:08:43 -0600

    Adjust layout and add test content to page

--- app/page.tsx
+++ app/page.tsx
@@ -68,8 +68,9 @@ export default function Page() {
         {<AcmeLogo />}
         </div>
       </div>
+      
       <div className="grid-cols-5 mt-4 bg-cover bg-no-repeat bg-center bg-clip-border p-5 bg-[url(/mayflower_gulch.jpg)] gap-4 md:flex-col">
-        <div className="col-start-1 col-end-3 justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="col-start-3 col-end-4 gap-4 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
             <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
               <strong>Welcome to my website.</strong> Here I will document and catalog my backcountry ski and snowboard trips.
             </p>
@@ -81,6 +82,7 @@ export default function Page() {
             </Link>
         </div>
         <div className="bg-black h-40 mt-4 gap-4 col-start-3 col-end-5">
+          <p className="text-xl text-white">Test words and extra stuff</p>
         </div>
       </div>
     </main>
