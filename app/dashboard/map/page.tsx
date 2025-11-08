06edeaa4a32e38105a6bf7ee554c416eb0515cb3
Legs21 <sjhawk02@gmail.com>
2025-11-08 13:48:01 -0700

    Added Map viewer with all routes added

--- app/dashboard/map/page.tsx
+++ app/dashboard/map/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <p>ArcGIS map to go here</p>;
+  return <div className='h-screen'>
+    <iframe width="100%" height="100%" allow="local-network-access; geolocation" title="Backcountry Routes 24-25" src="https://spencerhawk.maps.arcgis.com/apps/mapviewer/index.html?configurableview=true&webmap=855d714fe70643bca210a888bcf445e2&theme=light&bookmarks=true&heading=true&legend=true&scroll=false&extent=-11841330.955111548,4775895.881216867,-11762906.564091064,4826420.756913301,102100" ></iframe>
+  </div>;
 }
